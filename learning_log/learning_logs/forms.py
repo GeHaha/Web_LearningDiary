bcf573f80d9a1dfa917f13959fd8627b6fec8931
gehaha <2844785299@qq.com>
2018-07-06 15:32:35 +0800

    2018/7/6 19.1finish

--- learning_log/learning_logs/forms.py
+++ learning_log/learning_logs/forms.py
@@ -6,10 +6,19 @@ Created on Thu Jul  5 22:05:39 2018
 """
 
 from django import forms
-from .models import Topic
+from .models import Topic,Entry
+
 
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['text']
-        labels = {'text':''}
+        labels = {'text': ''}
+        
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels ={'text':''}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
+        
